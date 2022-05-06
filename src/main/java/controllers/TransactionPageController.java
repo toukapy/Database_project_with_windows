50890827188478ff990bb5c47e6e1759ee8277f2
Andy <amandasin258@gmail.com>
2022-05-06 11:41:47 +0200

    transactions first steps

--- src/main/java/controllers/TransactionPageController.java
+++ src/main/java/controllers/TransactionPageController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import uis.Controller;
 import uis.MainGUI;
 
@@ -17,6 +18,35 @@ public class TransactionPageController implements Controller {
     @Override
     public void initializeInformation() throws SQLException {
 
+    }
+
+
+    @FXML
+    void onClickAddCustomer(){
+        tranPageWin.showAddCustomer();
+    }
+    @FXML
+    void onClickDeleteCustomer(){
+        tranPageWin.showDeleteCustomer();
+    }
+    @FXML
+    void onClickSwapGuides(){
+        tranPageWin.showSwapGuides();
+    }
+    @FXML
+    void onClickUpdateTourguide(){
+        tranPageWin.showUpdateGuide();
+    }
+    @FXML
+    void onClickMakeOrder(){
+        tranPageWin.showMakeOrder();
+    }
+
+    @FXML
+    void onClickSales(){tranPageWin.showSales(); }
+    @FXML
+    void onClickBack(){
+        tranPageWin.showMain();
     }
 
 }
